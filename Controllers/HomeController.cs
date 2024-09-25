5cee878c16945b1cfb0f31e8e7dbb94ac2d6b78a
Selman Karabul <mselmankarabul@gmail.com>
2024-09-25 03:32:43 +0300

    BlogPost display on home & hero section

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MusicBlog.Web.Models;
+using MusicBlog.Web.Repositories;
 using System.Diagnostics;
 
 namespace MusicBlog.Web.Controllers
@@ -7,15 +8,19 @@ namespace MusicBlog.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            this.blogPostRepository = blogPostRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            return View(blogPosts);
         }
 
         public IActionResult Privacy()
