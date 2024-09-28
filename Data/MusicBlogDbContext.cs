4c3198543fa8f757f386187d528854f1ab91b437
Selman Karabul <mselmankarabul@gmail.com>
2024-09-28 14:46:25 +0300

    Like db update

--- Data/MusicBlogDbContext.cs
+++ Data/MusicBlogDbContext.cs
@@ -11,5 +11,7 @@ namespace MusicBlog.Web.Data
 
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<BlogPostLike> BlogPostLike { get; set; }
     }
 }
--- Models/Domain/BlogPost.cs
+++ Models/Domain/BlogPost.cs
@@ -16,5 +16,8 @@
 
         // Navigation property
         public ICollection<Tag> Tags { get; set; }
+
+        public ICollection<BlogPostLike> Likes { get; set; }
+
     }
 }
